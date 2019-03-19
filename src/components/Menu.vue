d14cf99f4c06f62d58af0bc65bd87b3449d47446
Rohan Lamb <lambrohan@gmail.com>
2019-03-19 19:17:45 +0530

    still bug in menu toggle

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,30 +1,122 @@
 <template>
   <div id="container">
-    <div class="bg"></div>
+    <div class="ham-icon" :class="{menuButtonActive:menuVisible,menuButtonInactive:!menuVisible}" @click="menuVisible = !menuVisible" @mouseenter="menuButtonMouseEnter" @mouseleave="menuButtonMouseLeave">
+      <div class="top"></div>
+      <div class="middle"></div>
+      <div class="bottom"></div>
+    </div>
+    <div v-if="menuVisible" class="menu">
+      <div class="bg"></div>
+    </div>
 
   </div>
 </template>
 <script>
+import anime from 'animejs';
 export default {
-  name:"Menu"
+  name:"Menu",
+  data(){
+    return{
+      menuVisible:false,
+    }
+  },
+  mounted(){
+
+
+  },
+  methods:{
+
+   
+    menuButtonMouseEnter:function(){
+      anime({
+        targets:'.ham-icon div',
+        margin:'6px 0',
+        duration:10,
+        ease:'easeInOutExpo',
+      })
+
+    },
+ 
+    menuButtonMouseLeave:function(){
+       anime({
+        targets:'.ham-icon div',
+        margin:'4.1px 0',
+        duration:10,
+        ease:'easeInOutExpo'
+      })
+      
+    }
+    
+  }
   
 }
 </script>
 <style lang="scss" scoped>
 #container{
   width:100%;
-  height: 100vh;
+  top:0;
+  z-index: 100;
   position: absolute;
+}
+.menu{
+  position: fixed;
+  width: 100%;
+  height:100vh;
+  top:-100%;
+  left:0;
 
   .bg{
     width: 100%;
     height:100%;
-    top:-100%;
+    top:0;
     left: 0;
-    position:fixed;
+    position: absolute;;
     background: #00ffff;
-    z-index: 100;
   }
+}
+
+.ham-icon{
+  position:fixed;
+  background: rgba(0, 255, 255, 0.877);
+  top:2rem;
+  right:2rem;
+  width:52px;
+  height:52px;
+  border-radius: 4px; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  div{
+    position: auto;
+    margin: 4px 0;
+    width: 60%;
+    height:4px;
+    background: #000;
+    border-radius: 20px;
+    transition: all 0.2s linear;
+  }
+}
+
+.ham-icon:hover{
+  cursor: pointer;
+}
+
+.menuButtonActive{
+  div{
+    position: absolute;
+    margin:0 0;
+    background: red;
+  }
+
+}
+.menuButtonInactive{
+  div{
+    position: absolute;
+    margin:0 0;
+  }
+
 }
 </style>
 
